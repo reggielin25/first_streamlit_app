7732c338e0b19a5b7513fc467c9a55334a6e1625
reggielin25 <137063416+reggielin25@users.noreply.github.com>
2023-06-19 19:48:52 +0800

    Update streamlit_app.py
    adding single-quotation for breakfast menu title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title(Breakfast Menu)
+streamlit.title('Breakfast Menu')
 streamlit('Omega 3 & Blueberry Outmeal')
 streamlit('Kale, Spinach & Rocket Smoothie')
 streamlit('Hard-Boiled Free-Range Egg')
