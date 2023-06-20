76d462b7257b86f80b4ef1de415ca6b0fd077685
reggielin25 <137063416+reggielin25@users.noreply.github.com>
2023-06-20 16:21:24 +0800

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ def insert_row_snowflake(new_fruit):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 if streamlit.button('Add a Fruit to the List'):
+   with my_cnx.cursor() as my_cur:
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
    streamlit.text(back_from_function)
