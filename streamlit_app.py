5409600fb179a3481cc699d41cb4651b33b3a55a
reggielin25 <137063416+reggielin25@users.noreply.github.com>
2023-06-20 15:34:11 +0800

    Update streamlit_app.py
    Added nested if with error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,16 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response.
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Kiwi")
-
-# take the json version of the response and normalize it 
+try
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a data table
 streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
 # Don't run anything past here while we trouleshot
 #streamlit.stop()
 #import snowflake.connector
